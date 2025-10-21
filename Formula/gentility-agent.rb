cc09f8fa00b4a2761ab386b4014fb3c1e822d2ab
James Tippett <james.tippett@gmail.com>
2025-10-21 20:55:00 +0700

    Release v1.1.9

--- Formula/gentility-agent.rb
+++ Formula/gentility-agent.rb
@@ -2,7 +2,7 @@ class GentilityAgent < Formula
   desc "Daemon for Gentility AI remote access"
   homepage "https://gentility.ai"
   url "https://github.com/gentility-ai/agent.git",
-      tag: "v1.1.8"
+      tag: "v1.1.9"
   license "MIT"
 
   depends_on "crystal" => :build
